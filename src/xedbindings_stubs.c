98895aa480ba2fac64530888564324ad54b72d92
Ryan Goulden <percontation@gmail.com>
2019-05-29 18:02:41 -0700

    bigger disasm buf

--- src/xedbindings_stubs.c
+++ src/xedbindings_stubs.c
@@ -7,7 +7,7 @@
 
 value xb_disassemble(value syntax_int, value decoded_inst, value addr) {
   CAMLparam3(syntax_int, decoded_inst, addr);
-  char buf[100];
+  char buf[480]; // big buffer cuz fully-explicit XED syntax can be verbose.
   xed_syntax_enum_t syntax = Int_val(syntax_int);
   const xed_decoded_inst_t *xedd = (void *)Nativeint_val(decoded_inst);
   xed_uint64_t address = Int64_val(addr);
