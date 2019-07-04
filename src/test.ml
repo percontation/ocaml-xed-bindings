8f63a6f5d723f13bd12e2f5f91a9e53b1b8aa2e8
Ryan Goulden <percontation@gmail.com>
2019-07-03 17:52:39 -0700

    frob tests

--- src/test.ml
+++ src/test.ml
@@ -1,20 +1,18 @@
 open Xed
-open Result
-
-let (>>=) a b = match a with Ok x -> b x | Error _ as y -> y
-let (>>|) a b = match a with Ok x -> Ok (b x) | Error _ as y -> y
 
 let main () =
   let eax = Enum.reg_of_string "EAX" |> Enum.reg_to_string in
   assert (eax = "EAX");
+
   let blank = OperandValues.to_string @@ OperandValues.init () in
   assert (blank = "");
-  let () = match decode state64 "\x55" with
-    | Ok x -> DecodedInst.to_string x |> print_endline; DecodedInst.disassemble x INTEL 0L |> print_endline
-    | Error x -> prerr_string "Error: "; prerr_endline (Enum.error_to_string x); assert false
-  in
+
+  let pushrbp = decode state64 "\x55" |> ok_err in
+  print_endline @@ DecodedInst.to_string pushrbp;
+  print_endline @@ DecodedInst.disassemble pushrbp INTEL 0L;
+
   let shortdecode = decode state32 "\x0f" in
   assert (shortdecode = Error Enum.BUFFER_TOO_SHORT);
   0
 
-let () = Pervasives.exit @@ main ()
+let () = exit @@ main ()
