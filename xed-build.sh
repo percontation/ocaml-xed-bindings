8ff32ef270df32675a33f559342242dacc565a6c
Ryan Goulden <percontation@gmail.com>
2022-11-10 16:31:26 -0800

    update readme, build real dllxed.so

--- xed-build.sh
+++ xed-build.sh
@@ -2,16 +2,23 @@
 set -ue
 cd "`dirname "$0"`"
 
-( cd xed && exec ./mfile.py --no-werror --extra-flags=-fpic --install-dir=kits/xed-install install )
+( cd xed && exec ./mfile.py --no-werror --shared --extra-flags=-fpic --install-dir=kits/xed-install install )
 
 OUT=src/bind/xedkit
 rm -Rf "$OUT"
 mkdir "$OUT"
 mkdir "$OUT/include" "$OUT/lib"
 cp -R xed/kits/xed-install/include/xed "$OUT/include/"
-cp -R xed/kits/xed-install/lib/libxed.a "$OUT/lib/"
+cp xed/kits/xed-install/lib/libxed.a "$OUT/lib/"
 
-# dune currently wants to see this, even though it never gets used
-touch "$OUT/lib/dllxed.so"
-
-dune build
+for dll in libxed.so libxed.dylib ""; do
+  dll="${dll+xed/kits/xed-install/lib/}$dll"
+  if test -f "$dll"; then
+    cp "$dll" "$OUT/lib/dllxed.so"
+    break
+  fi
+done
+if test -z "$dll"; then
+  # dllxed.so must be present, but most builds don't actually use it.
+  touch "$OUT/lib/dllxed.so"
+fi
