575a8b98eb3dd4caf606a8005bb858d64dd19c82
Ryan Goulden <percontation@gmail.com>
2022-11-29 07:45:46 -0800

    build reorg

--- xed-build.sh
+++ xed-build.sh
@@ -4,25 +4,26 @@ cd "`dirname "$0"`"
 
 (
   cd xed
-  ./mfile.py --no-werror --extra-flags=-fpic --install-dir=kits/xed-install-static install
-  ./mfile.py --no-werror --shared --install-dir=kits/xed-install-shared install
+  rm -Rf kits/xed-install
+  ./mfile.py --no-werror --extra-flags=-fpic --shared --install-dir=kits/xed-install install
+  ./mfile.py --no-werror --extra-flags=-fpic          --install-dir=kits/xed-install install
 )
 
 OUT=src/bind/xedkit
 rm -Rf "$OUT"
 mkdir "$OUT"
 mkdir "$OUT/include" "$OUT/lib"
-cp -R xed/kits/xed-install-static/include/xed "$OUT/include/"
-cp xed/kits/xed-install-static/lib/libxed.a "$OUT/lib/"
+cp -R xed/kits/xed-install/include/xed "$OUT/include/"
+cp xed/kits/xed-install/lib/libxed.a "$OUT/lib/"
 
 for dll in libxed.so libxed.dylib xed.dll ""; do
-  dll="${dll+xed/kits/xed-install-shared/lib/}$dll"
+  dll="${dll+xed/kits/xed-install/lib/}$dll"
   if test -f "$dll"; then
     cp "$dll" "$OUT/lib/dllxed.so"
     break
   fi
 done
-if test -z "$dll"; then
+if test -e "$OUT/lib/dllxed.so"; then :; else
   # Dune always requires this file, but native builds don't care.
   echo "Failed to build dllxed.so, continuing with an empty stub." >&2
   touch "$OUT/lib/dllxed.so"
